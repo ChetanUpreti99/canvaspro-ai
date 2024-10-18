a850a9cecfc0761705fae5020a888f9b217ec29d
ChetanUpreti99 <chetanupreti97@gmail.com>
2024-10-18 09:17:51 +0530

    feat: layout setup

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,48 +2,59 @@
 
 import { useEffect, useRef } from "react";
 
+
 import { fabric } from "fabric";
 
 import { useEditor } from "@/features/editor/hooks/useEditor";
+import Footer from "@/features/editor/components/footer";
+import Navbar from "@/features/editor/components/navbar";
+import Sidebar from "@/features/editor/components/sidebar";
+import Toolbar from "@/features/editor/components/toolbar";
 
 
 
 const Editor = () => {
 
-	const {init} = useEditor();
+	const { init } = useEditor();
 	const canvasWrapperRef = useRef<HTMLDivElement>(null);
 	const canvasRef = useRef(null);
 
 
 	useEffect(() => {
-		const canvas =  new fabric.Canvas(canvasRef.current,{
-			controlsAboveOverlay:true,
-			preserveObjectStacking:true
+		const canvas = new fabric.Canvas(canvasRef.current, {
+			controlsAboveOverlay: true,
+			preserveObjectStacking: true
 		});
 
 		init(
 			{
-				initialCanvasWrapper:canvasWrapperRef.current!,
-				initialCanvas:canvas,
+				initialCanvasWrapper: canvasWrapperRef.current!,
+				initialCanvas: canvas,
 			}
 		);
 	}, [init]);
 
-return (
-	<div className="h-full flex flex-col">
-
-	
-	<div className="h-full flex-1 bg-zinc-900" ref={canvasWrapperRef}>
-		 {
-		 	/* Canvas resizing is difficult hence we are using a div as canvasWrapperRef 
-		 	and we will use a resize observer to see if canvasWrapperRef is resized and use the width and height from there to set width 
-		 	and height of canvas */
-		 }
-		<canvas  ref={canvasRef} />
-		Canvas Pro Editor
-	</div>
-	</div>
-);
+	return (
+		<div className="h-full flex flex-col">
+			<Navbar />
+			<div className="flex absolute h-[calc(100%-56px)] w-full top-14">
+				<Sidebar />
+				<main className="flex relative overflow-auto bg-neutral-800 flex-1 flex-col">
+					<Toolbar/>
+					<div className="h-full flex-1 bg-zinc-900" ref={canvasWrapperRef}>
+						{
+							/* Canvas resizing is difficult hence we are using a div as canvasWrapperRef 
+							and we will use a resize observer to see if canvasWrapperRef is resized and use the width and height from there to set width 
+							and height of canvas */
+						}
+						<canvas ref={canvasRef} />
+						Canvas Pro Editor
+					</div>
+					<Footer/>
+				</main>
+			</div>
+		</div>
+	);
 }
 
 export default Editor;
---
+++ src/features/editor/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return <nav className="flex items-center w-full h-14 gap-4">
+    Navbar
+  </nav>;
+};
+
+export default Navbar;
